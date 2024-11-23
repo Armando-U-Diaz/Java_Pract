607c0d014e0c04d195c76fb937f538ea28987eb1
Diaz ArmandoU <diaz.armando.u@gmail.com>
2024-11-22 19:54:02 -0600

    Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+public class Factorial {
+    public static void main(String []arm) {
+        byte resp, num, x;
+        int fac;
+        do{
+            num=Byte.parseByte(JOptionPane.showInputDialog("Ingresa un numero"));
+            fac = 1;
+            for (x = num; x >= 1; x --) {
+            fac *= x;
+            System.out.print(fac + "\t");
+            }
+            System.out.println("");
+            resp=Byte.parseByte(JOptionPane.showInputDialog("Presiona 1 para continuar"));
+        } while(resp == 1);
+    }
+}
